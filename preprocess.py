2a0c3d48945ff26c0052ae92a553a9f244a4b041
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-04-23 19:54:15 +0400

    Add a preprocessing script for *.cfg.

---
+++ preprocess.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os, sys, re
+
+def process_dir_or_file(base, path, f_out):
+    if os.path.isdir(path):
+        print >>f_out, '# start ' + path
+        for child in os.listdir(path):
+            process_dir_or_file(base, os.path.join(path, child), f_out)
+        print >>f_out, '# end ' + path
+        return True
+    elif os.path.isfile(path):
+        print >>f_out, '# start ' + path
+        process_file(base, path, f_out)
+        print >>f_out, '# end ' + path
+        return True
+    else:
+        print >>f_out, "# WARNING: " + path + " not found"
+        return False
+
+def process_file(base, path, f_out):
+    f_in = open(path)
+    for line in f_in:
+        line = line.rstrip()
+        m = re.match(r'[ \t]*{([^{}]+)}[ \t]*', line)
+        if m:
+            include = m.group(1)
+            if not process_dir_or_file(base, os.path.join(base, include), f_out):
+                print >>f_out, line
+        else:
+            print >>f_out, line
+
+if __name__ == '__main__':
+    process_file(sys.argv[2], sys.argv[1], sys.stdout)
