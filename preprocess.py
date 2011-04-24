a6c64c1ecdede1da27e97582ea298d8ecc9a2f3c
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-04-25 01:32:09 +0400

    Ahem.
    As it turns out, you can not put random cr*p in comments in Wesnoth configs.
    Apparently, some of them aren't comments.
    Anyway, this seems like a dead end. We really need a working opendir(), or at least a way for fopen() to fail on directories.

--- preprocess.py
+++ preprocess.py
@@ -4,24 +4,26 @@ import os, sys, re
 
 def process_dir_or_file(base, path, f_out):
     if os.path.isdir(path):
-        print >>f_out, '# start ' + path
+#        print >>f_out, '# start ' + path
         for child in os.listdir(path):
             process_dir_or_file(base, os.path.join(path, child), f_out)
-        print >>f_out, '# end ' + path
+#        print >>f_out, '# end ' + path
         return True
     elif os.path.isfile(path):
-        print >>f_out, '# start ' + path
+#        print >>f_out, '# start ' + path
         process_file(base, path, f_out)
-        print >>f_out, '# end ' + path
+#        print >>f_out, '# end ' + path
         return True
     else:
-        print >>f_out, "# WARNING: " + path + " not found"
+#        print >>f_out, "# WARNING: " + path + " not found"
         return False
 
 def process_file(base, path, f_out):
     f_in = open(path)
     for line in f_in:
-        line = line.rstrip()
+	if line[-1] == '\n':
+	  line = line[:-1]
+#        line = line.rstrip()
         m = re.match(r'[ \t]*{([^{}]+)}[ \t]*', line)
         if m:
             include = m.group(1)
